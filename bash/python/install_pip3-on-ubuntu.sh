aa8ad4ca6301e45f7c357d66e907a8eef052c735
Tech-Overlord <am900820@gmail.com>
2018-08-01 16:09:25 +0500

    bash script added to install pip3 for python3 on ubuntu

---
+++ bash/python/install_pip3-on-ubuntu.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+## Note: I will be redoing this script to use if else statements to verify the version of python and install the pip accordingly (i.e. pip2 or pip3)
+
+# Install pip3 for python3
+sudo apt-get install python3-pip -y
