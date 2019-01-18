1a01e5c9e78af52432bb88e9d76b729faadb1d23
amuhammad <amuhammad@ctacorp.com>
2019-01-18 10:45:34 -0500

    Oracle Java8 URL mod to latest

--- bash/java/install_java-jdk8_x64.sh
+++ bash/java/install_java-jdk8_x64.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-## THIS SCRIPT WAS SCRIPTED WITH THE CONSIDERATION THAT IT WILL BE RUN USING "root" USER. So please switch to root user to execute it.
+# SCRIPT WAS SCRIPTED WITH THE CONSIDERATION THAT IT WILL BE RUN USING "root" USER. So please switch to root user to execute it.
 ## This script automatically installs and sets environment variables for JAVA JDK.
 ## Please note that the URL in the wget command will need to be modified prior to execution if a newer version has been released by Oracle. So, please check Oracle's website prior to execution of the script or simply paste replace the URL with the latest x64 version of Oracle Java 8's JDK tar.gz file available on their web. You can find it here: http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
 
@@ -16,7 +16,7 @@ echo "Current working in the following directory: $(pwd)"
 echo
 
 # Download Oracle's JAVA JDK 8u181. Latest version of Java 8 available at the time of this script's inception. If there is a newer version, then you need to replace the http URL with that of the latest version from Oracle Java's website.
-wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
+wget --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz
 
 ## Assign the tar downloaded as a varlue to a variable for use in the script.
 tar_name=`ls jdk-*`
@@ -71,4 +71,4 @@ environment_file=`cat /etc/environment`
 echo
 echo "The contents of /etc/environment file are the following: "
 echo "$environment_file"
-echo 
+echo
